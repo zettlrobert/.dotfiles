80c05137920cecdd524aad8a5a980db325fe315e
zettlrobert <zettl.robert@gmail.com>
2021-05-08 19:52:54 +0200

    setup setup direcotry in ~

--- setup/setup.sh
+++ setup/setup.sh
@@ -16,6 +16,7 @@ echo -e "Setup Script starting...\n"
 #      "kitty"           # GPU based Terminal
 #      "ranger"          # Filemanager
 #      "ripgrep"         # Recursively searches directories for a regex pattern
+#      "bottom"          # A cross-platform graphical process/system monitor
 #      "xclip"           # command line interface to X selections  
 #      "timeshift"       # System restore utility
 #      "neofetch"        # Shows Linux System Information 
@@ -83,13 +84,54 @@ do
     fi
 done
 
+# Create directorys in $HOME
+mkdir $HOME/bin $HOME/desktop $HOME/documents $HOME/downloads $HOME/music $HOME/pictures $HOME/public $HOME/templates $HOME/videos $HOME/development
+
+    if [ $? = 0 ]
+    then
+        echo -e "${green}OK${NC}: create base $HOME folder structure"
+    else
+        echo -e "${red}Fail${NC}: failed creating directorys in $HOME"
+    fi
+
+# Remove capital directorys in $HOME
+rm -r $HOME/Desktop $HOME/Documents  $HOME/Downloads $HOME/Music $HOME/Pictures $HOME/Public $HOME/Templates $HOME/Videos
+
+# Update ~ structure to lower case
+xdg-user-dirs-update
+
+    if [ $? = 0 ]
+    then
+        echo -e "${green}OK${NC}: Updated xdg-user-dirs"
+    else
+        echo -e "${red}Fail${NC}: xdg-user-dirs updating"
+    fi
+
+
+# Intall all Packages which are not availalbe in package repository.
+
+## Bottom Resource Monitor
+function install_bottom () {
+  sudo curl -LO https://github.com/ClementTsang/bottom/releases/download/0.5.7/bottom_0.5.7_amd64.deb
+  sudo dpkg -i bottom_0.5.7_amd64.deb
+  rm bottom_0.5.7_amd64.deb
+}
+
+install_bottom
+
+    if [ $? = 0 ]
+    then
+        echo -e "${green}OK${NC}: bottom installed"
+    else
+        echo -e "${red}Fail${NC}: installing bottom"
+    fi
+
 # Update Default Terminal to Kitty
 sudo update-alternatives --config x-terminal-emulator
 
-
 # Setup Audofiles audio configuration, pulseeffect
-sudo cat ./default-configurations/default-audio-daemon/daemon.conf > /etc/pulse/daemon.conf
-    if [ $? = 0 ]
+# sudo cat ./default-configurations/default-audio-daemon/daemon.conf > /etc/pulse/daemon.conf
+   if [ $? = 0 ]
     then
         echo -e "${green}OK${NC}: /etc/pulse/daemon.conf successfully configured"
     else
